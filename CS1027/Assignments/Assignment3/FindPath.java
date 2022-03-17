2c9f466258b756b78900a09ff0db0e298a434cc4
Arsalaan Ali <arsalaanali2@gmail.com>
2022-03-17 19:40:29 -0400

    bestchamber

--- CS1027/Assignments/Assignment3/FindPath.java
+++ CS1027/Assignments/Assignment3/FindPath.java
@@ -9,23 +9,34 @@ public class FindPath {
             System.out.println(e);
         }
     }
-    public DLStack path(){
 
+    public DLStack path() {
+        
     }
 
     public Map getMap(){
         return pyramidMap;
     }
     
-    public boolean isDim(Chamber currentChamber){
-        if(currentChamber == null || currentChamber.isSealed() || currentChamber.isLighted()){
+    public boolean isDim(Chamber currentChamber) {
+        if (currentChamber == null || currentChamber.isSealed() || currentChamber.isLighted()) {
             return false;
         }
-        for(int i = 0; i<6; i++){
-            if(currentChamber.getNeighbour(i) != null && currentChamber.getNeighbour(i).isLighted()){
+        for (int i = 0; i < 6; i++) {
+            if (currentChamber.getNeighbour(i) != null && currentChamber.getNeighbour(i).isLighted()) {
                 return true;
             }
         }
         return false;
+    }
+    
+    public Chamber bestChamber(Chamber currentChamber) {
+        for (int i = 0; i < 6; i++) {
+            Chamber neighbor = currentChamber.getNeighbour(i);
+            if (neighbor.isTreasure() && neighbor.isMarked()) {
+                return neighbor;
+            }
+        }
+        return null;
     }
 }
