bd1aa47ea5af86ef849152c358204e3717879075
Arsalaan Ali <arsalaanali2@gmail.com>
2022-10-13 15:06:11 -0400

    Lab5

--- CS2211/Labs/Lab3.c
+++ CS2211/Labs/Lab3.c
@@ -12,11 +12,15 @@ int main()
     minInt = int1 < int2 ? int1 : int2;
     minInt = minInt < int3 ? minInt : int3;
     minInt = minInt < int4 ? minInt : int4;
-    printf("Number = %d %d\n", maxInt, minInt);
+    printf("Highest Number = %d \n", maxInt);
+    printf("Lowest Number = %d \n", minInt);
+
 
     int daysInMonth, startingDay;
-    printf("Enter length of month and starting day: ");
-    scanf("%d %d", &daysInMonth, &startingDay);
+    printf("Enter number of days in month: ");
+    scanf("%d", &daysInMonth);
+    printf("Enter starting day of the week (1=Sun, 7=Sat): ");
+    scanf("%d", &startingDay);
     for (int i = 0; i<startingDay-1; i++){
         printf("   ");
     }
